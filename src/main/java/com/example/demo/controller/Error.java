f7571bb3ac0c4d169b69da378b09733da99c189d
AaronLi963 <zy.li0824@gmail.com>
2026-05-08 14:58:01 +0800

    error

--- src/main/java/com/example/demo/controller/Error.java
+++ src/main/java/com/example/demo/controller/Error.java
@@ -2,6 +2,5 @@ package com.example.demo.controller;
 
 public class Error {
     public static final int ERROR_CODE_INTERNAL_ERROR = 1001;
-    public static final int ERROR_CODE_INVALID_REQUEST = 1002;
-    public static final int ERROR_CODE_USER_NOT_FOUND= 1003;
+    public static final int ERROR_CODE_INVALID_INPUT = 1002;
 }
