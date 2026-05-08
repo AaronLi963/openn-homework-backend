77cbd8078a9d04fa6918907a5ddc265c3b316cf4
AaronLi963 <zy.li0824@gmail.com>
2026-05-08 14:56:45 +0800

    delete user points

--- src/main/java/com/example/demo/repository/PointRepository.java
+++ src/main/java/com/example/demo/repository/PointRepository.java
@@ -6,9 +6,12 @@ import java.util.List;
 import com.example.demo.service.dto.LeaderboardDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
 
     @Query("SELECT p FROM Point p WHERE p.userId = :userId")
@@ -22,4 +25,8 @@ public interface PointRepository extends JpaRepository<Point, Long> {
             "GROUP BY p.userId " +
             "ORDER BY SUM(p.amount) DESC")
     List<LeaderboardDto> findLeaderboard(Pageable pageable);
+
+    @Modifying
+    @Query("DELETE FROM Point p WHERE p.userId = :userId")
+    void deleteByUserId(@Param("userId") String userId);
 }
